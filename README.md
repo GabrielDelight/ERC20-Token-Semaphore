14a5b7444fa6f84da2252664c77a7060bfbd7a6f
Gabriel Delight <gabrieldelight08@gmail.com>
2023-06-16 13:38:33 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# Sample Hardhat Project
+# ERC20 token Project
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
 
 ```shell
-npx hardhat help
+npm install
+npx hardhat compile 
 npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+npx hardhat run --network sepolia scripts/deploy.js
 ```
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,13 +8,13 @@ contract MyToken is ERC20Capped, ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
 
-    constructor(uint256 cap, uint256 reward)
-        ERC20("MyToken", "MYT")
-        ERC20Capped(cap * (10**decimals()))
-    {
+    constructor(
+        uint256 cap,
+        uint256 reward
+    ) ERC20("MyToken", "MYT") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
-        _mint(owner, 50000000 * (10**decimals()));
-        blockReward = reward * (10**decimals()); // Setting block reward for first deploy
+        _mint(owner, 50000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals()); // Setting block reward for first deploy
     }
 
     // Setting miner reward
@@ -38,11 +38,10 @@ contract MyToken is ERC20Capped, ERC20Burnable {
         super._beforeTokenTransfer(from, to, value);
     }
 
-    function _mint(address account, uint256 amount)
-        internal
-        virtual
-        override(ERC20Capped, ERC20)
-    {
+    function _mint(
+        address account,
+        uint256 amount
+    ) internal virtual override(ERC20Capped, ERC20) {
         require(
             ERC20.totalSupply() + amount <= cap(),
             "ERC20Capped: cap exceeded"
@@ -57,7 +56,7 @@ contract MyToken is ERC20Capped, ERC20Burnable {
 
     // Set block rewards
     function setBlockReward(uint256 reward) public onlyOwner {
-        blockReward = reward * (10**decimals());
+        blockReward = reward * (10 ** decimals());
     }
 
     //reusable modifier
